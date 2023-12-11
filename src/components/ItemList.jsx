9a9c55ec444d0217ddb2fc6598845ee999e5796a
Shaban Daja <shabandaja710@gmail.com>
2023-12-11 15:07:27 +0100

    Load a message when the list is empty

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,17 +1,20 @@
+import EmptyView from './EmptyView';
+
 export default function ItemList({
   items,
   handleDeleteItem,
   handleToggleItem,
 }) {
   return (
-    <ul>
+    <ul className='item-list'>
+      {items.length === 0 && <EmptyView />}
       {items.map((item) => {
         return (
           <Item
             key={item.id}
             item={item}
-            handleDeleteItem={handleDeleteItem}
-            handleToggleItem={handleToggleItem}
+            onDeleteItem={handleDeleteItem}
+            onToggleItem={handleToggleItem}
           />
         );
       })}
@@ -19,18 +22,18 @@ export default function ItemList({
   );
 }
 
-function Item({ item, handleDeleteItem, handleToggleItem }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li className='item'>
       <label>
         <input
-          onChange={() => handleToggleItem(item.id)}
+          onChange={() => onToggleItem(item.id)}
           checked={item.packed}
           type='checkbox'
         />{' '}
         {item.name}
       </label>
-      <button onClick={() => handleDeleteItem(item.id)}>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
