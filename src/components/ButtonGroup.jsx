111bcf9364f445768744cc74c8c1bc69aafab3fe
Shaban Daja <shabandaja710@gmail.com>
2023-12-11 11:12:28 +0100

    DRY buttons by mapping over an array.

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,12 +1,26 @@
 import Button from './Button';
 
+const secondaryButtons = [
+  'Mark all as complete',
+  'Mark all as incomplete',
+  'Reset to initial',
+  'Remove all items',
+];
+
 export default function ButtonGroup() {
   return (
     <section className='button-group'>
-      <Button type='secondary'>Mark all as complete</Button>
+      {secondaryButtons.map((text) => {
+        return (
+          <Button key={text} type='secondary'>
+            {text}
+          </Button>
+        );
+      })}
+      {/* <Button type='secondary'>Mark all as complete</Button>
       <Button type='secondary'>Mark all as incomplete</Button>
       <Button type='secondary'>Reset to initial</Button>
-      <Button type='secondary'>Remove all items</Button>
+      <Button type='secondary'>Remove all items</Button> */}
     </section>
   );
 }
